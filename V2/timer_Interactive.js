8d343449c24c9340398e58e3813b34f171f36491
Archit80 <archittaneja80@gmail.com>
2024-07-12 13:49:02 +0530

    Revert "Enter to submit"
    This reverts commit 2d30b0e5a021cf608b36f511235b5aac565a73f4.

--- V2/timer_Interactive.js
+++ V2/timer_Interactive.js
@@ -1,19 +1,13 @@
 
+
 function clock(){
 
-const Button = document.querySelector("#submit");
+    const Button = document.querySelector("#submit");
 const baapFrame = document.querySelector(".baapFrame");
 
 Button.addEventListener("click", () => {
     baapFrame.classList.remove("hide");
     Button.classList.add("remove");
-  });
-
-  window.addEventListener("keydown", (e) => {
-    if(e.code == "Enter"){
-    baapFrame.classList.remove("hide");
-    Button.classList.add("remove");
-    }
   });
   
  
@@ -34,16 +28,8 @@ Button.addEventListener("click", () => {
     
     // const dateControl = document.querySelector('input[type="date"]').value;
     // console.log(dateControl);
-    let difference = pickDate() - currentDate;
+    const difference = pickDate() - currentDate;
     console.log(difference);
-
-
-    if (difference <= 0) {
-        console.log("Countdown complete!");
-        baapFrame.innerHTML = "Time's Up";
-        Button.classList.remove("remove");
-       }
-  
     
     var daysLeft = Math.floor(difference / (1000 * 60 * 60 * 24));
     var hoursLeft = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -51,17 +37,20 @@ Button.addEventListener("click", () => {
     var secondsLeft = Math.floor((difference % (1000 * 60)) / 1000);
    
    
-    Days = document.querySelector("#Days");
-    Hours = document.querySelector("#Hours");
-    Minutes = document.querySelector("#Minutes");
-    Seconds = document.querySelector("#Seconds");
-
-
     Days.innerText = daysLeft;
     Hours.innerText = hoursLeft;
     Minutes.innerText = minutesLeft;
     Seconds.innerText = secondsLeft;
 
+
+    Days = document.querySelector("#Days");
+    Hours = document.querySelector("#Hours");
+    Minutes = document.querySelector("#Minutes");
+    Seconds = document.querySelector("#Seconds");
+
+    if (difference <= 0) {
+        console.log("Countdown complete!");
+    }
   
 }
 
